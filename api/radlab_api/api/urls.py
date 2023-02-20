b46de4bf22f3682b7a0c4499380bb1f56b90cbae
Jeffrey <jeffrey.luo@mail.utoronto.ca>
2023-02-20 15:54:53 -0500

    added retrieval apis

--- api/radlab_api/api/urls.py
+++ api/radlab_api/api/urls.py
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
+from django.urls import path
 from django.urls import path, include
 from api.views import (
+    CreateBreathingDataView,
     ListBreathingDataView,
     RetrieveBreathingDataView
 )
 
 urlpatterns = [
+    path('data/create/', CreateBreathingDataView.as_view()),
     path('data/all', ListBreathingDataView.as_view()),
     path('data/<int:pk>', RetrieveBreathingDataView.as_view()),
-=======
-from django.urls import path
-from views import CreateTrialData
-
-urlpatterns = [
-    path('data/save/', CreateTrialData.as_view())
->>>>>>> 19ed793c532a7807fdc0addcd879e9ff2f70c2ee
 ]
--- api/radlab_api/api/views.py
+++ api/radlab_api/api/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import generics
-from serializers import BreathingDataSerializer
-# Create your views here.
+from api.models import BreathingData
+from api.serializers import BreathingDataSerializer
 
-class CreateTrialData(generics.CreateAPIView):
+# radlab api views
+
+# Create breathing data
+class CreateBreathingDataView(generics.CreateAPIView):
     serializer_class = BreathingDataSerializer
+
+
+# List all breathing data
+class ListBreathingDataView(generics.ListAPIView):
+    queryset = BreathingData.objects.all()
+    serializer_class = BreathingDataSerializer
+
+
+# Retrieve specific breathing data by id
+class RetrieveBreathingDataView(generics.RetrieveAPIView):
+    queryset = BreathingData.objects.all()
+    serializer_class = BreathingDataSerializer
+    lookup_field = 'pk'
+
+    def get_object(self):
+        return get_object_or_404(BreathingData, id=self.kwargs['pk'])
+
