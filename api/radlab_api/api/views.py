c0e014eb7e279b058e438f81808429c32e2100a2
jeremiahwong007 <73967834+jeremiahwong007@users.noreply.github.com>
2023-02-20 15:13:27 -0500

    django views and new save_trial file

--- api/radlab_api/api/views.py
+++ api/radlab_api/api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from serializers import BreathingDataSerializer
 # Create your views here.
+
+class CreateTrialData(generics.CreateAPIView):
+    serializer_class = BreathingDataSerializer
---
+++ save_trial.py
@@ -0,0 +1,2 @@
+file = open("raw.txt", "r")
+
